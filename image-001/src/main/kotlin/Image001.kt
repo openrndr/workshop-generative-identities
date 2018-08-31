08cadefe3f51d43058a4e13c314455e1f3fbac2a
BoydRotgans <boyd@rndr.studio>
2018-08-31 16:04:30 +0200

    Merge branch 'master' of https://github.com/openrndr/workshop-generative-identities
    # Conflicts:
    #	settings.gradle

--- image-001/src/main/kotlin/Image001.kt
+++ image-001/src/main/kotlin/Image001.kt
@@ -3,6 +3,7 @@ import org.openrndr.application
 import org.openrndr.configuration
 import org.openrndr.draw.ColorBuffer
 import org.openrndr.math.Vector2
+import org.openrndr.math.map
 import org.openrndr.shape.Rectangle
 
 
@@ -14,7 +15,7 @@ class Image001 : Program() {
     override fun setup() {
         super.setup()
 
-        image = ColorBuffer.fromFile("image-001/data/IMG_4124.JPG")
+        image = ColorBuffer.fromFile("data/IMG_4124.JPG")
     }
 
     override fun draw() {
@@ -30,10 +31,10 @@ class Image001 : Program() {
                     val pos1 = mouse.position
                     val pos2 = Vector2(x * blockSize , y * blockSize)
 
-                    val distance = Math.sqrt((pos1.x-pos2.x)*(pos1.x-pos2.x) + (pos1.y-pos2.y)*(pos1.y-pos2.y));
+                    val distance = Math.sqrt((pos1.x-pos2.x)*(pos1.x-pos2.x) + (pos1.y-pos2.y)*(pos1.y-pos2.y))
 
                     drawer.image(image,
-                            Rectangle(Vector2(x * blockSize + (distance*1.0), y * blockSize + (distance*1.0) ), blockSize, blockSize),
+                            Rectangle(Vector2(x * blockSize + (distance), y * blockSize + (distance) ), blockSize, blockSize),
                             Rectangle(Vector2(x * blockSize , y * blockSize ), blockSize, blockSize))
                 }
             }
