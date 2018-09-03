70c85bf000c1fd2877aa0a97a41dadafc906eeb5
Edwin Jakobs <edwin@rndr.studio>
2018-09-03 08:54:12 +0200

    Added basic003

--- basic-003/src/main/kotlin/Basic003.kt
+++ basic-003/src/main/kotlin/Basic003.kt
@@ -4,22 +4,33 @@ import org.openrndr.Program
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.configuration
-import org.openrndr.draw.*
-import org.openrndr.ffmpeg.FFMPEGVideoPlayer
 import org.openrndr.math.Vector2
-import org.openrndr.shape.Rectangle
-import org.openrndr.svg.loadSVG
-import org.openrndr.text.Writer
-import java.io.File
+import org.openrndr.shape.contour
 
 /**
  * OPENRNDR Basics 003
  * -------------------
- * Constructs a contour
+ * Constructs a contour, also uses sub() to get a sub contour
  */
 class Basic003 : Program() {
     override fun draw() {
         drawer.background(ColorRGBa.PINK)
+
+        val contour = contour {
+            moveTo(Vector2(100.0, 100.0))
+            lineTo(Vector2(200.0, 100.0))
+            lineTo(Vector2(200.0,200.0))
+            lineTo(Vector2(100.0, 200.0))
+            lineTo(Vector2(100.0, 100.0))
+            close()
+        }
+
+        drawer.stroke = ColorRGBa.BLACK
+        drawer.fill = null
+        drawer.contour(contour)
+
+        drawer.translate(200.0, 0.0)
+        drawer.contour(contour.sub(seconds, seconds + 0.1))
 
     }
 }
