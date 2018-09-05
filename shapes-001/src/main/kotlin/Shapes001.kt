7324391c99ace2675fa2c71f4b9cfa4e7ff6aa04
Edwin Jakobs <edwin@rndr.studio>
2018-09-05 13:10:57 +0200

    Added shapes-001 example

---
+++ shapes-001/src/main/kotlin/Shapes001.kt
@@ -0,0 +1,57 @@
+package org.openrndr.workshop
+
+import org.openrndr.Program
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.configuration
+import org.openrndr.svg.loadSVG
+import java.io.File
+
+class Shapes001 : Program() {
+    var drawFunc = {}
+
+    override fun setup() {
+        val letters = loadSVG(File("data/logo.svg").readText()).findShapes()
+
+        val shapes = loadSVG(File("data/shapes.svg").readText()).findShapes()
+
+        drawFunc = {
+            drawer.background(ColorRGBa.PINK)
+
+            for ((letterIndex, node) in letters.withIndex()) {
+
+                drawer.pushStyle()
+                drawer.pushTransforms()
+                drawer.fill = ColorRGBa.BLACK
+                drawer.shape(node.shape)
+                drawer.drawStyle.clip = node.bounds
+
+                drawer.stroke = ColorRGBa.PINK
+                drawer.fill = ColorRGBa.PINK
+                drawer.strokeWeight = 2.0
+
+
+                val shapeNode = shapes[letterIndex%shapes.size]
+
+                drawer.translate(node.bounds.center)
+                drawer.rotate(seconds*45.0)
+                drawer.translate(-shapeNode.bounds.center)
+
+                drawer.shape(shapeNode.shape)
+                drawer.popStyle()
+                drawer.popTransforms()
+            }
+        }
+    }
+
+    override fun draw() {
+        drawFunc()
+    }
+}
+
+fun main(args: Array<String>) {
+    application(Shapes001(), configuration {
+        width = 1280
+        height = 720
+    })
+}
